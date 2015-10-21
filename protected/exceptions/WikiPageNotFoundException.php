8cc19023d32d174e5993813aa56fb0c6d292d644
panchangyun <panchangyun@tuniu.com>
2015-10-21 18:25:05 +0800

    fix 404

--- protected/exceptions/WikiPageNotFoundException.php
+++ protected/exceptions/WikiPageNotFoundException.php
@@ -2,7 +2,7 @@
 
 
 class WikiPageNotFoundException extends Exception {
-    public function __construct($repoPath, $pageName, $prevException=null){
+    public function __construct($pageName, $repoPath, $prevException=null){
         parent::__construct(sprintf("Wiki page '%s' not found in '%s'.", $repoPath, $pageName), 1, $prevException);
     }
 } 
