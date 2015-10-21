561c06dd83f1d82c36753ba38d775358705446cf
panchangyun <panchangyun@tuniu.com>
2015-10-21 18:26:37 +0800

    update about

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -5,6 +5,6 @@ $this->breadcrumbs=array(
 );
 ?>
 <h1>About</h1>
-
-<p>This is a "static" page. You may change the content of this page
-by updating the file <tt><?php echo __FILE__; ?></tt>.</p>
+<p>
+    This is a wiki using org files(see org-mode in emacs).
+</p>
