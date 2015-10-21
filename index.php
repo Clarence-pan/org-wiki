594ac6afd25d4d719357bbaf89b928cb94041f67
panchangyun <panchangyun@tuniu.com>
2015-10-21 19:46:30 +0800

    FIX ERROR TYPES

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ defined('YII_DEBUG') or define('YII_DEBUG', $_GET['debug'] === 'enable' or $_COO
 
 if (YII_DEBUG){
     ini_set('display_errors', 'on');
-    error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE ^ E_DEPRECATED);
 }
+
+error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE ^ E_DEPRECATED);
 
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
