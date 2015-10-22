99fb00a14dece22ec1575e98e384b9d0d53cad1f
panchangyun <panchangyun@tuniu.com>
2015-10-22 18:53:08 +0800

    add another parser for org-mode

--- protected/components/html/parser/org/Parser.php
+++ protected/components/html/parser/org/Parser.php
@@ -5,13 +5,14 @@ namespace html\parser\org;
 
 
 use html\dom\DivElement;
+use html\dom\TextElement;
 
 class Parser {
 
     public function parse($text){
         $div = new DivElement(['class' => 'org-mode-page']);
 
-
+        $div->add(new TextElement("This is a test!"));
 
         return $div;
     }
