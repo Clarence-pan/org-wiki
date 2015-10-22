66770b8854fce0850b3495a1ec07c37d64605ec0
panchangyun <panchangyun@tuniu.com>
2015-10-22 19:03:29 +0800

    add another parser for org-mode

--- protected/components/html/parser/org/Parser.php
+++ protected/components/html/parser/org/Parser.php
@@ -12,7 +12,8 @@ class Parser {
     public function parse($text){
         $div = new DivElement(['class' => 'org-mode-page']);
 
-        $div->add(new TextElement("This is a test!"));
+        $div->add(new TextElement("This is a test! The follow are raw code:"));
+        $div->add(new CodeBlockElement(['lang' => 'org', 'code' => $text]));
 
         return $div;
     }
