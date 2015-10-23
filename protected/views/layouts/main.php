0a788e7004362e38dacba7ca1e274b90e07bf721
panchangyun <panchangyun@tuniu.com>
2015-10-23 13:44:07 +0800

    list all

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -37,8 +37,6 @@
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/wiki/view', 'pageName' => 'index.org')),
                 array('label'=>'List', 'url'=>array('/wiki/index')),
-//				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-//				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
